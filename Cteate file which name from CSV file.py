acd0ac557fe1214c9a2386b2b4a44b002c1425cc
Glasslightpiano <63344363+Glasslightpiano@users.noreply.github.com>
2021-08-27 16:09:24 +0800

    Add files via upload

---
+++ Cteate file which name from CSV file.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------
+# Open and read .csv file data
+# Create file and file name from .csv file data
+# Created on: 2021-08-27 15:53:05
+# -------------------------------------------------------------------
+
+# Import model.
+import csv
+import os
+
+# Read CSV file. 
+with open('D:\\ArcMap test\\TestReadSV.csv', newline='') as csvfile:
+    line = csv.reader(csvfile)
+
+    # Set name from CSV file and check.
+    for row in line:
+        outfile = str(row)
+        print(eval(outfile.strip('[]')))
+
+        # Create file on specify path.
+        os.mkdir('D:\\ArcMap test\\' + eval(outfile.strip('[]')))
+
+print('Create File Finish!')
